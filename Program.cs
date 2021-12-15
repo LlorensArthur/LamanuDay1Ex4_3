5074f3d8f318226c69acd84084b40c74a7a67ceb
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:22:45 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+List<int> numberList = new List<int>();
+numberList.Add(1);
+numberList.Add(2);
+numberList.Add(3);
+numberList.Add(4);
+int result = 0;
+foreach (var item in numberList)
+{
+    result += item;
+}
+Console.Write("Voici les nombres qui vont être calculés : ");
+foreach (var item in numberList)
+{
+    Console.Write($"{item}, ");
+}
+Console.WriteLine();
+Console.WriteLine($"Le résultat de ces nombres est {result}");
