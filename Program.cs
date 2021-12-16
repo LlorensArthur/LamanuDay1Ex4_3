2039663fa30d8b7edcbe3734f0780939818c526d
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-16 11:09:22 +0100

    Ajouté des commentaires pour le programmeur

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿List<int> numbers = new List<int>();
+// Ajout manuel de nombres
 numbers.Add(1);
 numbers.Add(2);
 numbers.Add(3);
 numbers.Add(4);
+// Addition de la liste des nombres 
 int result = 0;
 foreach (var item in numbers)
 {
     result += item;
 }
+// Affichage de l'opération pour l'utilisateur
 Console.Write("Voici les nombres qui vont être calculés : ");
 for (int i = 0; i < numbers.Count; i++)
 {
